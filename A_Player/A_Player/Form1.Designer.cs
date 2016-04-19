a80613a5ecb4cb4160e677995809632d570e9b44
4erDak <Nik_i_ta98@mail.ru>
2016-04-19 13:57:24 +0300

    Added files via upload

---
+++ A_Player/A_Player/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace A_Player
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button2 = new System.Windows.Forms.Button();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.toolStripTextBox1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.button1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripTextBox4 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DimGray;
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button2.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(324, 97);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(31, 31);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.BackgroundImageChanged += new System.EventHandler(this.button2_Click);
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.AutoSize = false;
+            this.trackBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.trackBar1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar1.Enabled = false;
+            this.trackBar1.Location = new System.Drawing.Point(202, 134);
+            this.trackBar1.Maximum = 0;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(318, 29);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(242, 265);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(201, 183);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "0:00:00";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(468, 183);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "0:00:00";
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.DimGray;
+            this.button3.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button3.BackgroundImage")));
+            this.button3.Enabled = false;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.Lime;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(370, 97);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(34, 31);
+            this.button3.TabIndex = 6;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(335, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Volume";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.trackBar2.Enabled = false;
+            this.trackBar2.Location = new System.Drawing.Point(263, 217);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(210, 45);
+            this.trackBar2.TabIndex = 9;
+            this.trackBar2.TickStyle = System.Windows.Forms.TickStyle.TopLeft;
+            this.trackBar2.Value = 10;
+            this.trackBar2.Scroll += new System.EventHandler(this.trackBar2_Scroll);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(479, 230);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Max";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(231, 230);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(26, 13);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "Min";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(335, 265);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(0, 13);
+            this.label11.TabIndex = 16;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripTextBox1,
+            this.toolStripTextBox2,
+            this.toolStripTextBox3,
+            this.toolStripTextBox4});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(728, 24);
+            this.menuStrip1.TabIndex = 17;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // toolStripTextBox1
+            // 
+            this.toolStripTextBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripTextBox1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.button1});
+            this.toolStripTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripTextBox1.Name = "toolStripTextBox1";
+            this.toolStripTextBox1.Size = new System.Drawing.Size(48, 20);
+            this.toolStripTextBox1.Text = "Файл";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.button1.ForeColor = System.Drawing.Color.Lime;
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(152, 22);
+            this.button1.Text = "Открыть...";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // toolStripTextBox2
+            // 
+            this.toolStripTextBox2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripTextBox2.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripTextBox2.Name = "toolStripTextBox2";
+            this.toolStripTextBox2.Size = new System.Drawing.Size(79, 20);
+            this.toolStripTextBox2.Text = "Настройки";
+            // 
+            // toolStripTextBox3
+            // 
+            this.toolStripTextBox3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripTextBox3.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripTextBox3.Name = "toolStripTextBox3";
+            this.toolStripTextBox3.Size = new System.Drawing.Size(65, 20);
+            this.toolStripTextBox3.Text = "Справка";
+            // 
+            // toolStripTextBox4
+            // 
+            this.toolStripTextBox4.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.toolStripTextBox4.ForeColor = System.Drawing.Color.Lime;
+            this.toolStripTextBox4.Name = "toolStripTextBox4";
+            this.toolStripTextBox4.Size = new System.Drawing.Size(53, 20);
+            this.toolStripTextBox4.Text = "Выход";
+            this.toolStripTextBox4.Click += new System.EventHandler(this.toolStripTextBox4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.ClientSize = new System.Drawing.Size(728, 401);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.menuStrip1);
+            this.Font = new System.Drawing.Font("Verdana", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.ForeColor = System.Drawing.Color.Lime;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Audio Player";
+            this.TransparencyKey = System.Drawing.Color.Transparent;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseDown);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            this.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseUp);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox1;
+        private System.Windows.Forms.ToolStripMenuItem button1;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox2;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox3;
+        private System.Windows.Forms.ToolStripMenuItem toolStripTextBox4;
+    }
+}
+
---
+++ A_Player/A_Player/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using WMPLib;
+using TagLib;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace A_Player
+{
+    public partial class Form1 : Form
+    {
+        WindowsMediaPlayer wmp;
+
+        bool pausePlay;
+        bool mousePressed = false;
+        Point mouseDownPos;
+        string fileName;
+        int trackInfo = 0;
+        bool left = true;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            wmp = new WindowsMediaPlayer();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var ofd = new OpenFileDialog();
+            if ((ofd.ShowDialog() == DialogResult.OK) && (ofd.FileName != string.Empty))
+            {
+                wmp.URL = ofd.FileName;
+                wmp.controls.play();
+                trackBar1.Enabled = true;
+                trackBar2.Enabled = true;
+                timer1.Enabled = true;
+                timer1.Interval = 1000;
+                label1.Text = "Сейчас играет:";
+                button1.Enabled = false;
+                button2.Enabled = true;
+                button3.Enabled = true;
+                ShowInTaskbar = false;
+                TopMost = true;
+
+                var file = File.Create(ofd.FileName);
+                fileName = file.Tag.FirstPerformer + " - " + file.Tag.Title;
+                label11.Text = fileName;
+
+                FormBorderStyle = FormBorderStyle.None;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           
+            pausePlay = !pausePlay;
+            if (pausePlay)
+            {
+                button2.Image = Image.FromFile("N:\\Универ\\Групповая динамика\\A_Player\\A_Player\\pause.bmp");
+                wmp.controls.pause();
+                label1.Text = "Пауза:";
+                
+            }
+            if (!pausePlay)
+            {
+                button2.Image = Image.FromFile("N:\\Универ\\Групповая динамика\\A_Player\\A_Player\\play.bmp");
+                wmp.controls.play();
+                label1.Text = "Сейчас играет:";  
+            }
+            
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            wmp.controls.stop();
+            button1.Enabled = true;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            trackBar1.Value = 0;
+            trackBar2.Value = 10;
+            trackBar1.Enabled = false;
+            trackBar2.Enabled = false;
+            timer1.Enabled = false;
+            label1.Text = "";
+            label11.Text = "";
+            label3.Text = "0:00:00";
+            label2.Text = "0:00:00";
+            ShowInTaskbar = true;
+            TopMost = false;
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+        }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            wmp.controls.currentPosition = trackBar1.Value;
+        }
+
+        private void trackBar2_Scroll(object sender, EventArgs e)
+        {
+            if (trackBar2.Value == 10)
+                wmp.settings.volume = 100;
+            if (trackBar2.Value == 9)
+                wmp.settings.volume = 90;
+            if (trackBar2.Value == 8)
+                wmp.settings.volume = 80;
+            if (trackBar2.Value == 7)
+                wmp.settings.volume = 70;
+            if (trackBar2.Value == 6)
+                wmp.settings.volume = 60;
+            if (trackBar2.Value == 5)
+                wmp.settings.volume = 50;
+            if (trackBar2.Value == 4)
+                wmp.settings.volume = 40;
+            if (trackBar2.Value == 3)
+                wmp.settings.volume = 30;
+            if (trackBar2.Value == 2)
+                wmp.settings.volume = 20;
+            if (trackBar2.Value == 1)
+                wmp.settings.volume = 10;
+            if (trackBar2.Value == 0)
+                wmp.settings.volume = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            trackBar1.Maximum = Convert.ToInt32(wmp.currentMedia.duration);
+            trackBar1.Value = Convert.ToInt32(wmp.controls.currentPosition);
+
+            if (wmp != null)
+            {
+                int s = (int)wmp.currentMedia.duration;
+                int h = s / 3600;
+                int m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                label3.Text = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+
+                s = (int)wmp.controls.currentPosition;
+                h = s / 3600;
+                m = (s - (h * 3600)) / 60;
+                s = s - (h * 3600 + m * 60);
+                label2.Text = String.Format("{0:D}:{1:D2}:{2:D2}", h, m, s);
+            }
+            else
+            {
+                label3.Text = "0:00:00";
+                label2.Text = "0:00:00";
+            }
+
+            if (trackInfo <= fileName.Length)
+            {
+                timer1.Interval = 250;
+                if (left)
+                    label11.Text = fileName.Substring(trackInfo, fileName.Length - trackInfo);
+                else
+                    label11.Text =  fileName.Substring(fileName.Length - trackInfo, trackInfo);
+                trackInfo++;
+            }
+            else
+            {
+                trackInfo = 0;
+                if (left) left = false;
+                else left = true;
+            }
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Form1 f1 = new Form1();
+            f1.Focus();
+            mousePressed = true;
+            mouseDownPos = new Point(f1.Location.X + e.Location.X, f1.Location.Y + e.Location.Y + SystemInformation.CaptionHeight);
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (mousePressed)
+            {
+                if (e.Button == MouseButtons.Left)
+                {
+                    if (FormBorderStyle == FormBorderStyle.None)
+                    {
+                        Left = Cursor.Position.X - mouseDownPos.X;
+                        Top = Cursor.Position.Y - mouseDownPos.Y + 22;
+                    }
+                    else
+                    {
+                        Left = Cursor.Position.X - mouseDownPos.X - 3;
+                        Top = Cursor.Position.Y - mouseDownPos.Y - 3;
+                    }
+                }
+            }
+           
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            mousePressed = false;
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripTextBox4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
